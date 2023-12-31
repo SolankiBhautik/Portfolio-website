69aae4fcc223a6669a5a0e9f2b352f128e89a5a6
Bhautik <solankibhautik1702@gmail.com>
2023-12-31 23:50:09 +0530

    for assets error 8

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,11 @@
 import logo from './Brand-logo.png'
 import arrow from './arrow.svg'
-import profile from './Profile.png'
 import thumbnail from './Project cover.png'
 import quots from './quots.png'
 import menu from './menu.svg'
 import close from './close.svg'
 import laptop from './laptop bg.jfif'
+import thumbnail from '/public/images/Project cover.png'
 
 export {
     logo,
