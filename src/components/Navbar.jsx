38724364b85da993171783a158a551b97c4ea512
Bhautik <solankibhautik1702@gmail.com>
2024-01-02 23:43:53 +0530

    styling bug fixed 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
     
     return (
-        <nav className='flex flex-row justify-between items-center z-10 my-4'>
+        <nav className='flex flex-row justify-between items-center z-10 my-1 sm:my-4 '>
             <div>
                 <Link to="/"><img src={logo} alt="Brand-logo" className='w-[200px] md:m-0 m-3'/> </Link>
             </div>
