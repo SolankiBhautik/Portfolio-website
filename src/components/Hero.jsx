7507ee36a835d22219c7915696c7fd83edda0cfa
Bhautik <solankibhautik1702@gmail.com>
2024-01-02 23:41:31 +0530

    styling bug fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { HashLink } from 'react-router-hash-link';
 
 function Hero() {
     return (
-        <div className='flex flex-col justify-center items-center min-h-[80vh]'>
+        <div className='flex flex-col justify-center items-center min-h-[80vh] mt-10'>
             <h1 className='heading1 relative'>Bridging Dimensions: <br className='' />Merging Web Development with the Power of 3D</h1>
             <p className='paragraph text-center text-sm mt-2'>Code meets clay, pixels meet polygons. This is where the boundaries of web design break down and magic unfolds. Forget flat screens, I sculpt digital playgrounds where 3D artistry meets smooth interactions. It's not just design, it's an invitation to explore. Dive deeper than the scroll, experience web design reimagined. Are you ready to cross the bridge? My portfolio awaits.</p>
             <HashLink to="/#project" className='button1 mt-8'>Explore works</HashLink>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,10 @@ import { HashLink } from 'react-router-hash-link';
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
     
-
     return (
-        <nav className='navbar flex flex-row justify-between items-center z-10'>
+        <nav className='flex flex-row justify-between items-center z-10 my-4'>
             <div>
-                <Link to="/"><img src={logo} alt="Brand-logo" className='w-[125px] h-[22px] md:m-0 m-3'/> </Link>
+                <Link to="/"><img src={logo} alt="Brand-logo" className='w-[200px] md:m-0 m-3'/> </Link>
             </div>
             <div className={`z-10 navigation md:flex  items-center gap-8 ${isOpen ? 'open' : ' '}`}>
                 <Link to="/" className='text-[14px] paragraph'>HOME</Link>
@@ -18,7 +17,7 @@ const Navbar = () => {
                 <Link to="/AboutMe"  className='text-[14px] paragraph'>ABOUT ME</Link>
                 <Link to="/CantactMe" className='text-[14px] paragraph'>CONTECT ME</Link>
             </div>
-            <div className='md:hidden z-10 fixed top-0 right-0 p-5 rounded-bl-3xl bg-[#0000007c] backdrop-blur-[20px]'>
+            <div className='md:hidden z-10 fixed top-0 right-0 p-5 rounded-bl-3xl bg-[#0000007c] backdrop-blur-[10px]'>
                 {
                     isOpen ? (<img src={close} alt="close menu" onClick={() => setIsOpen(false)}/>) : (
                         <img src={menu} alt="open menu" onClick={() => setIsOpen(true)}/>
